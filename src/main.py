9402bda7a9e78a3ed3ee8cab283f0cb6e05ea1a5
Nix Atsu <atsux@protonmail.com>
2018-02-28 21:43:42 -0500

    spaced repetition basic - mongo crud operations

--- src/main.py
+++ src/main.py
@@ -1,17 +1,42 @@
 import os
-from flask import Flask, render_template
+
+from datetime import date, timedelta
+from flask import Flask, render_template, request, url_for
 from flask_pymongo import PyMongo
+from werkzeug.utils import redirect
+
 
 app = Flask(__name__,
             static_url_path='/static')
-app.config.from_pyfile('config.py')
+app.config.from_pyfile('config.py', silent=True)
 mongo = PyMongo(app)
 
+DAY_REPEATS = [0, 1, 8, 16, 35, 70]
 
-@app.route('/')
+
+def add_days(days):
+    return (date.today() + timedelta(days=days)).isoformat()
+
+
+@app.route('/', methods=['GET', 'POST'])
 def root():
-    items = mongo.db.items.find()
-    return render_template('spaced_repetition.html', items=items)
+    if request.method == 'POST':
+        item = request.form.to_dict(flat=True)
+
+        for interval in DAY_REPEATS:
+            item["added_days_ago"] = interval
+            mongo.db.repeats.update(
+                {'date': add_days(interval)},
+                {'$push': {'items': item}},
+                upsert=True
+            )
+        return redirect(url_for('root'))
+
+    else:
+        db_result = mongo.db.repeats.find_one(
+            {'date': date.today().isoformat()}
+        )
+        return render_template('spaced_repetition.html', result=db_result)
 
 
 if __name__ == '__main__':
